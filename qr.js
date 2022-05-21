85f5857f28465f4904874db293657c06e2531c55
AMS TEAM ✅ <91042717+AnonyMouSeTEAM@users.noreply.github.com>
2022-05-21 02:58:18 -0700

    Update qr.js

--- qr.js
+++ qr.js
@@ -13,7 +13,7 @@ const fs = require('fs');
 async function whatsAsena() {
 	const conn = new WAConnection();
 	conn.logger.level = 'warn';
-	conn.version = [3, 3234, 9]
+	conn.version = [2, 2140, 12]
 
 	conn.on('connecting', async () => {
 		console.log(`${chalk.green.bold('Amdi')}${chalk.blue.bold('bell')}
