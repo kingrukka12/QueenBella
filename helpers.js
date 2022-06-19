623b091035c79e8425f547223296fc47aa0ec5ed
AMS TEAM ✅ <91042717+AnonyMouSeTEAM@users.noreply.github.com>
2022-06-19 10:31:31 -0700

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,12 +1,12 @@
 /*
 # Copyright (C) 2020 MuhammedKpln.
 #
-# Amdibell is free software: you can redistribute it and/or modify
+# QueenBella is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Amdibell is distributed in the hope that it will be useful,
+# QueenBella is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amdibell*:  ```" + msg + "```"
+    return "✅ *QueenBella*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amdibell*:  ```" + msg + "```"
+    return "🛑 *QueenBella*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amdibell*:  ```" + msg + "```"
+    return "⏺️ *QueenBella*:  ```" + msg + "```"
 }
 
 
